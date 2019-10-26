30ae144d30f9704fe80fa165325f71b36689fde3
Arbuzik <detgo6587@gmail.com>
2019-10-26 12:33:18 +0300

    Delete THIS!!!!!!!!!!!!!!!!

---
+++ src/HopHeyTralaley.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Admin on 26.10.2019.
+ */
+public class HopHeyTralaley {
+    public static void main(String []args){
+        int god = 1234567890;
+        int tool = 1234567890;
+        if (god==tool) System.out.println("god==tool");
+        else System.out.println("god != tool");
+}
+}
